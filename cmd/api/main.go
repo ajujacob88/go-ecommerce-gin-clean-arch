8e44c57c8dd5dedb0d529edef8d2104ee77eda51
ajujacob <ajujacob88@gmail.com>
2023-05-10 18:32:59 +0530

    Initial commit after doing config

---
+++ cmd/api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ajujacob88/go-ecommerce-gin-clean-arch/pkg/config"
+)
+
+func main() {
+	config, configErr := config.LoadConfig()
+	if configErr != nil {
+		log.Fatal("cannot load config: ", configErr)
+	}
+}
---
+++ pkg/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBName     string `mapstructure:"DB_NAME"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+}
+
+// to hold all names of env variables
+var envs = []string{
+	"DB_HOST", "DB_NAME", "DB_USER", "DB_PORT", "DB_PASSWORD", // database
+}
+
+func LoadConfig() (Config, error) {
+	var config Config
+
+	viper.AddConfigPath("./")
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+
+	for _, env := range envs {
+		if err := viper.BindEnv(env); err != nil {
+			return config, err
+		}
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return config, err
+	}
+
+	if err := validator.New().Struct(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
