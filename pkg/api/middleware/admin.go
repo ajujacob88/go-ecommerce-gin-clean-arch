915594b143d3b96b6b17faefce732ef9673f5af9
ajujacob <ajujacob88@gmail.com>
2023-05-23 12:23:42 +0530

    product category added

--- pkg/api/middleware/admin.go
+++ pkg/api/middleware/admin.go
@@ -10,15 +10,20 @@ import (
 
 func AdminAuth(c *gin.Context) {
 	tokenString, err := c.Cookie("AdminAuth")
-	fmt.Println("check1")
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, res.ErrorResponse(400, "failed to login1", err.Error(), nil))
+	if err == http.ErrNoCookie {
+		c.JSON(http.StatusUnauthorized, res.ErrorResponse(400, "failed to login1", "AdminAuth cookie not present", nil))
+		c.Abort() // Stop the execution of subsequent handlers
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, res.ErrorResponse(500, "failed to login - internal server error", err.Error(), nil))
+		c.Abort() // Stop the execution of subsequent handlers
 		return
 	}
 	fmt.Println("check2")
 	adminID, err := ValidateToken2(tokenString)
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, res.ErrorResponse(400, "failed to login2", err.Error(), nil))
+		c.Abort() // Stop the execution of subsequent handlers
 		return
 	}
 	c.Set("adminID", adminID)
