950a2ae7c6e0d52b835196b63f24f3f5396e404f
ajujacob <ajujacob88@gmail.com>
2023-06-13 11:13:37 +0530

    combining the place order handler

--- pkg/api/handler/payment.go
+++ pkg/api/handler/payment.go
@@ -27,19 +27,6 @@ func NewPaymentHandler(paymentUseCase services.PaymentUseCase, orderUseCase serv
 	}
 }
 
-// CreateRazorpayPayment
-// @Summary Users can make payment using razor pay checkout
-// @ID create-razorpay-payment
-// @Description Users can make payment via Razorpay after placing orders
-// @Tags Payment
-// @Accept json
-// @Produce json
-// @Param order_id path string true "Order id"
-// @Success 200 {object} response.Response
-// @Failure 422 {object} response.Response
-// @Failure 400 {object} response.Response
-// @Failure 500 {object} response.Response
-// @Router /user/payments/razorpay/{order_id} [get]
 func (cr *PaymentHandler) RazorpayCheckout(c *gin.Context, orderInfo domain.Order, cartItems []domain.CartItems) {
 
 	orderInfo.OrderStatusID = 1 //order pending ... first order pending , then after razor pay verifcation, set order status to placed
@@ -51,6 +38,7 @@ func (cr *PaymentHandler) RazorpayCheckout(c *gin.Context, orderInfo domain.Orde
 	}
 	fmt.Println("razorpayorderid is", razorpayOrderID, "and total order value is", orderInfo.OrderTotalPrice)
 
+	// create the order as order pending, cart clearing and orderline only adter razor pay verification
 	createdOrder, err := cr.orderUseCase.SaveOrder(c.Request.Context(), orderInfo, cartItems)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, response.ErrorResponse(500, "failed to save the order", err.Error(), nil))
@@ -128,6 +116,16 @@ func (cr *PaymentHandler) RazorpayVerify(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, response.ErrorResponse(500, "failed to update payment details", err.Error(), nil))
 
 	}
+
+	//noe clear the cart and create orderline
+	/*
+		err = cr.orderUseCase.OrderLineAndClearCart(c.Request.Context(), createdOrder, cartItems)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, response.ErrorResponse(500, "failed to save the orderline and clear cart", err.Error(), nil))
+			return
+		}
+	*/
+
 	c.JSON(http.StatusAccepted, response.SuccessResponse(202, "payment success", nil))
 
 }
@@ -181,6 +179,77 @@ func (cr *PaymentHandler) RazorpayCheckout(c *gin.Context) {
 		"email":    "smartstore@gmail.com",
 		"phone_no": "7733333333",
 	})
+
+}
+
+
+
+
+// Now razor pay verify/ payment success updations
+// Razorpay verify
+// @Summary Handling successful payment
+// @ID payment-success
+// @Description updating payment details upon successful payment
+// @Tags Payment
+// @Accept json
+// @Produce json
+// @Param razorpay_payment_id query string true "provide the razorpay_payment_id"
+// @Param razorpay_order_id query string true "provide the razorpay_order_id"
+// @Param razorpay_signature query string true "provide the razorpay_signature"
+// @Param order_id query string true "provide the order_id"
+// @Param order_total query string true "provide the order_total"
+// @Success 202 {object} response.Response
+// @Failure 401 {object} response.Response
+// @Failure 400 {object} response.Response
+// @Failure 500 {object} response.Response
+// @Router /user/payments/success/ [post]
+func (cr *PaymentHandler) RazorpayVerify(c *gin.Context) {
+	razorpayPaymentID := c.Query("razorpay_payment_id")
+	razorpayOrderID := c.Query("razorpay_order_id")
+	razorpaySignature := c.Query("razorpay_signature")
+	userorderID := c.Query("order_id")
+	orderID, err := strconv.Atoi(userorderID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.ErrorResponse(400, "unable to fetch order id", err.Error(), nil))
+		return
+	}
+	razorpayOrderTotal := c.Query("order_total")
+	total, err := strconv.ParseFloat(razorpayOrderTotal, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.ErrorResponse(400, "unable to fetch user id from context", err.Error(), nil))
+		return
+	}
+	userID, err := handlerutil.GetUserIdFromContext(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, response.ErrorResponse(401, "unable to fetch user id from context", err.Error(), nil))
+		return
+	}
+
+	// verify the razorpay payment (by using signature as described by https://razorpay.com/docs/payments/server-integration/go/payment-gateway/build-integration)
+	err = verify.VerifyRazorpayPayment(razorpayOrderID, razorpayPaymentID, razorpaySignature)
+	if err != nil {
+		response := response.ErrorResponse(400, "failed to verify razorpay payment", err.Error(), nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	paymentVerifier := request.PaymentVerification{
+		UserID:          userID,
+		OrderID:         orderID,
+		RazorpayOrderID: razorpayOrderID,
+		PaymentRef:      razorpayPaymentID,
+		Total:           total,
+	}
+
+	err = cr.paymentUseCase.UpdatePaymentDetails(c.Request.Context(), paymentVerifier)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, response.ErrorResponse(500, "failed to update payment details", err.Error(), nil))
+
+	}
+
+
+
+	c.JSON(http.StatusAccepted, response.SuccessResponse(202, "payment success", nil))
 
 }
 */
